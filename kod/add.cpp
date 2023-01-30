4b43f09ad035da17e01b2c042890cb9058110848
Kolxa <karolxd1000@gmail.com>
2023-01-30 18:08:14 +0100

    Prosty program i funkcja

---
+++ kod/add.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include "add.h"
+
+int add(int x,int y) {
+    return x+y;
+}
---
+++ kod/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+
+int main()
+{
+    int a=5,b=23;
+    cout << add(a,b) << endl;
+    return 0;
+}
